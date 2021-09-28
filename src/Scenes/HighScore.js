5fdd227e8eb9e4368a2d8378fda6ed3dd7a02cce
Wei Lun <garyganweilun@gmail.com>
2021-09-29 03:09:15 +0800

    Change highscore logic

--- src/Scenes/HighScore.js
+++ src/Scenes/HighScore.js
@@ -1,15 +1,20 @@
 $(() => {
 
-    let dodgePipeScore = localStorage.getItem("easyHighScore");
-    let collectStarScore = localStorage.getItem("normalHighScore");
-    let dodgeBirdScore = localStorage.getItem("hardHighScore");
-    const $easyScore = $('<h1>').text(dodgePipeScore);
-    const $normalScore = $('<h1>').text(collectStarScore);
-    const $hardScore= $('<h1>').text(dodgeBirdScore);
 
-    $("#dodgepipes").append($easyScore);
-    $("#collectstars").append($normalScore);
-    $("#dodgebirds").append($hardScore);
+    setInterval(() => {
+        let dodgePipeScore = localStorage.getItem("easyHighScore");
+        let collectStarScore = localStorage.getItem("normalHighScore");
+        let dodgeBirdScore = localStorage.getItem("hardHighScore");
+        $("#dodgepipes").empty();
+        $("#dodgepipes").append(dodgePipeScore);
+        $("#collectstars").empty();
+        $("#collectstars").append(collectStarScore);
+        $("#dodgebirds").empty();
+        $("#dodgebirds").append(dodgeBirdScore);
+    }, 1000);
+   
+
+   
 
 
 });
